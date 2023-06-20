a8d9c244c06c1da308b33a816f1f1f4a3d926f86
pl9632008 <67965957+pl9632008@users.noreply.github.com>
2023-06-20 20:36:17 +0800

    Update DeepNet.cpp

--- src/DeepNet.cpp
+++ src/DeepNet.cpp
@@ -6,79 +6,43 @@
 #define _access access
 
 // char * trt_model_stream896_= new char[1109182076];
-
 // char * trt_model_stream640_= new char[152546830];
-
-
 
 void DeepNet::loadEngine(std::string str){
         size_t size{0};
         std::ifstream file(str, std::ios::binary);
+        char * trt_model_stream{nullptr};
         if(file.good()){
             file.seekg(0,std::ios::end);
             size = file.tellg();
             file.seekg(0,std::ios::beg);
-            // //trt_model_stream = new char[size];
-            // file.read(trt_model_stream,size);
-            // file.close();
+            trt_model_stream = new char[size];
+            file.read(trt_model_stream,size);
+            file.close();
         
 // The engine plan file is generated on an incompatible device, expecting compute 5.3
-
-         if(str=="./engine/model3_cls24_896_lm_20230310.engine"){
-                // file.read(trt_model_stream896_,size);
-                // file.close();
-                // runtime = createInferRuntime(logger);
-                // engine = runtime->deserializeCudaEngine(trt_model_stream896_,size);
-                // context = engine->createExecutionContext();
-                // delete[] trt_model_stream896_;
-                
+         if(str=="./engine/model896.engine"){
+                runtime896_ = createInferRuntime(logger);
+                engine896_ = runtime896_->deserializeCudaEngine(trt_model_stream,size);
+                context896_ = engine896_->createExecutionContext(); 
             }else{
-                char * trt_model_stream640_= new char[size];
-
-                file.read(trt_model_stream640_,size);
-                file.close();
                 runtime640_ = createInferRuntime(logger);
-                engine640_ = runtime640_->deserializeCudaEngine(trt_model_stream640_,size);
+                engine640_ = runtime640_->deserializeCudaEngine(trt_model_stream,size);
                 context640_ = engine640_->createExecutionContext();
-                delete[] trt_model_stream640_;
             }
         }
-
-
-     
-        
-        // runtime = createInferRuntime(logger);
-        // engine = runtime->deserializeCudaEngine(trt_model_stream,size);
-        // context = engine->createExecutionContext();
-        // delete[] trt_model_stream;
-        //     if(str=="./engine/model3_cls24_896_lm_20230310.engine"){
-        //         file.read(trt_model_stream,size);
-        //         file.close();
-        //         runtime = createInferRuntime(logger);
-        //         engine = runtime->deserializeCudaEngine(trt_model_stream,size);
-        //         context = engine->createExecutionContext();
-        //         delete[] trt_model_stream;
-                
-        //     }else{
-        //         file.read(trt_model_stream640_,size);
-        //         file.close();
-        //         runtime = createInferRuntime(logger);
-        //         engine = runtime->deserializeCudaEngine(trt_model_stream640_,size);
-        //         context = engine->createExecutionContext();
-        //         delete[] trt_model_stream640_;
-        //     }
-        // }
-   
-       
+        delete[] trt_model_stream;     
 }
 
 bool DeepNet::load_model(){
+
     std::string model_path="./engine/";
-    // std::string ForeignBody_model_path=model_path+"model3_cls24_896_lm_20230310.engine";
+
+    // std::string ForeignBody_model_path=model_path+"model896.engine";
     // LOGI("<load model>load ForeignBody model");
     // if(_access(ForeignBody_model_path.c_str(), 0) != -1){
     // if(engine896_==nullptr){
-    //         loadEngine(ForeignBody_model_path,runtime896_,engine896_,context896_);
+    //         loadEngine(ForeignBody_model_path);
     //         if(engine896_==nullptr){
     //             LOGE("load model failed");
     //             return false;
@@ -87,9 +51,9 @@ bool DeepNet::load_model(){
     // }
     // LOGI("<load model>load ForeignBody model successful!");
 
-    //GroundWater
+    // //GroundWater(use yolov5s.engine)
     LOGI("<load model>load GroundWater model");
-    std::string GroundWater_model_path=model_path+"dmjs_640_20220316.engine";
+    std::string GroundWater_model_path=model_path+"model640.engine";
     if(_access(GroundWater_model_path.c_str(), 0) != -1){
        if(engine640_==nullptr){
             loadEngine(GroundWater_model_path);
